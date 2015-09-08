53ba3682e2fcac3a29f50c05e1eeb82911fc1e0e
stevehwg <shihwei.hwang@gmail.com>
2015-09-08 12:08:35 -0400

    test git push after modify on clone

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from .models import Post
 from .forms import PostForm
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('-published_date')[:5]
+    posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('-published_date')[:10]
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, post_id):
